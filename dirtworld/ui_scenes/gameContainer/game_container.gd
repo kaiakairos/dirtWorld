d315e33150a963822c8746a91e5fb081777b6371
kaia <kairoskaia815@gmail.com>
2025-10-07 23:32:10 -0500

    dithered lighting

--- dirtworld/ui_scenes/gameContainer/game_container.gd
+++ dirtworld/ui_scenes/gameContainer/game_container.gd
@@ -1,9 +1,36 @@
 extends Node2D
+class_name GameContainer
+
+## Objects ##
+@onready var world :World = $ViewportContainer/Viewport/World
+@onready var viewport :SubViewport = $ViewportContainer/Viewport
+@onready var lightViewport :SubViewport = $LightViewportContainer/LightViewport
+@onready var lightDrawer :LIGHTDRAWER = $LIGHTDRAWER
+	# Debug
+@onready var fpsLabel :Label = $fpsLabel
+
+# Lighting
+var lightRenderSize :int = 64
 
 func _ready() -> void:
 	get_viewport().size_changed.connect(changeWindowSize)
-
+	
+	#world.setLightMask(viewport.get_texture())
+	lightDrawer.material.set_shader_parameter("lightingMask",viewport.get_texture())
+	
+func _process(delta: float) -> void:
+	fpsLabel.text = "fps: " + str( int( 1.0 / delta ) )
+	
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	
 func changeWindowSize():
 	var rect :Rect2 = get_viewport_rect()
 	var width :int= int(rect.size.x)
-	#lightRender = (width / 8) + 14
+	viewport.size.x = width
+	lightViewport.size.x = width
+	lightRenderSize = (width / 8) + 14
--- dirtworld/world_scenes/world/world.gd
+++ dirtworld/world_scenes/world/world.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name World
 
 @export var worldContainer :WORLDCONTAINER
 var tickTimer :float = 0.0
@@ -6,20 +7,16 @@ var tick :int = 0 # the game loop tick
 const TICKRATE :int = 15
 
 var positionLastFrame :Vector2i = Vector2i.ZERO # debug
-var renderDistance :Vector2i = Vector2i(8,5) # test commit :)
+var renderDistance :Vector2i = Vector2i(8,5)
 
-# light 
-var lightRender :int = 64
+@onready var gameContainer :GameContainer = get_parent().get_parent().get_parent()
 
 func _ready() -> void:
 	worldContainer.setBlockContainer(BlockManager.blockContainer)
 	worldContainer.initializeArray(32,32)
 	worldContainer.debugWorldGen()
-	
-	get_viewport().size_changed.connect(changeWindowSize)
 
 func _process(delta: float) -> void:
-	
 	
 	# all debug shit
 	var dir :Vector2 = Vector2.ZERO
@@ -27,18 +24,12 @@ func _process(delta: float) -> void:
 	dir.y = Input.get_axis("ui_up","ui_down")
 	$testCamera.position += dir * 120 * delta
 	
-	$testCamera/Label.text = "fps: " + str( int( 1.0 / delta ) )
-	
-	
 	var trackingPosition = Vector2i($testCamera.global_position)
 	trackingPosition = trackingPosition/64
 	if positionLastFrame != trackingPosition:
 		worldContainer.chunkLoadArea(trackingPosition.x,trackingPosition.y,renderDistance.x,renderDistance.y)
 		worldContainer.unloadChunks(trackingPosition.x,trackingPosition.y,renderDistance.x,renderDistance.y)
 		print(trackingPosition)
-		# debug light update
-		#$LIGHTDRAWER.position = trackingPosition * 64
-		
 		
 		positionLastFrame = trackingPosition
 		
@@ -48,15 +39,24 @@ func _process(delta: float) -> void:
 		tickTimer -= 1.0 / float(TICKRATE)
 		gameTick(delta)
 	
+	drawLighting()
+
+func drawLighting() -> void:
 	var l :Vector2i = (positionLastFrame * 8) - Vector2i(28,20)
-	$LIGHTDRAWER.drawLight($WORLDCONTAINER,l.x + (32 - (lightRender/2)),l.y,lightRender,50)
-	
+	var size :int = gameContainer.lightRenderSize
+	var worldCoords :Vector2i = Vector2i(l.x + (32 - (size/2)), l.y )
+	gameContainer.lightDrawer.drawLight(worldContainer,worldCoords.x,worldCoords.y,size,50)
+	var pos :Vector2 = worldCoords * 8
+	var rect :Rect2 = get_viewport_rect()
+	var width :int= int(rect.size.x)
+	gameContainer.lightDrawer.position = pos - getTargetPosition() + Vector2(width/2,150)
+	gameContainer.lightDrawer.position.x = roundi(gameContainer.lightDrawer.position.x)
+	gameContainer.lightDrawer.position.y = roundi(gameContainer.lightDrawer.position.y)
+	print(gameContainer.lightDrawer.position)
 
 func gameTick(_delta:float) -> void:
 	tick += 1
 	worldContainer.simulateLoadedChunks(tick)
 
-func changeWindowSize():
-	var rect :Rect2 = get_viewport_rect()
-	var width :int= int(rect.size.x)
-	lightRender = (width / 8) + 14
+func getTargetPosition() -> Vector2:
+	return $testCamera.position
